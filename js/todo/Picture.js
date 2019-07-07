5ed4c2ed963c7cede878a404432198e0f175a733
Asmodey <Asmodey2007@tut.by>
2019-07-07 21:58:07 +0300

    Рефакторинг класса Picture;

--- js/todo/Picture.js
+++ js/todo/Picture.js
@@ -1,25 +1,17 @@
-(function () {
-    function Picture(options) {
+function Picture(options) {
+    this.container = document.getElementById(options.container);
+    this.imageUrl = options.imageUrl;
+    this.textDescription = options.textDescription;
 
-        options.container.innerHTML = createPicture(options.src, options.textPicture);
-
-
-        function createPicture(src, text) {
-            return `<div class="col text-center kabanStyle">
-                        <img src="img/Z6W8.gif">
-                        <img src=${src}>
-                        <p>${text}</p>
-                    </div>`
-        }
+    this.render = function() {
+        this.container.innerHTML = `<div class="col text-center kabanStyle">
+                                        <img src="img/Z6W8.gif">
+                                        <img src=${this.imageUrl}>
+                                        <p>${this.textDescription}</p>
+                                    </div>`
     }
+}
 
-    new Picture({
-        container: document.getElementById("componentKoban"),
-
-        src: "img/KvDTLVW1n6s.jpg",
-        textPicture: "уважаемому кабану посвящается"
-    });
-}());
 
 
 
