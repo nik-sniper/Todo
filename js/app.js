3a30892fa6c14fe0d96c1d9dcf87f57fc39f662c
Nik_Sniper <nik_mozir@mail.ru>
2019-07-11 10:48:54 +0300

    Улучшен Picture

--- js/app.js
+++ js/app.js
@@ -5,7 +5,14 @@ new Todo({
 let kabanPicture = new Picture({
     container: "componentKoban",
     imageUrl: "img/KvDTLVW1n6s.jpg",
-    textPicture: "уважаемому Кабану посвящается"
+    textPicture: "уважаемому Кабану посвящается",
+    callback: function (imgUrl, textImg) {
+        return `<div class="col text-center kabanStyle">
+                    <img src="img/Z6W8.gif">
+                    <img src=${imgUrl}>
+                    <p>${textImg}</p>
+                </div>`
+    }
 });
 
 let natashaPicture = new Picture({
--- js/todo/Picture.js
+++ js/todo/Picture.js
@@ -3,14 +3,18 @@ class Picture {
         this.container = document.getElementById(options.container);
         this.imageUrl = options.imageUrl;
         this.textDescription = options.textPicture;
+        this.callback = options.callback;
     }
 
+    defaultRender() {
+        return `<div class="col text-center kabanStyle">
+                    <img src="img/Z6W8.gif">
+                    <img src=${this.imageUrl}>
+                    <p>${this.textDescription}</p>
+                </div>`
+    }
 
     render() {
-        this.container.innerHTML += `<div class="col text-center kabanStyle">
-                                        <img src="img/Z6W8.gif">
-                                        <img src=${this.imageUrl}>
-                                        <p>${this.textDescription}</p>
-                                    </div>`
+        this.container.innerHTML += this.callback ? this.callback(this.imageUrl, this.textDescription) : this.defaultRender();
     }
 }
