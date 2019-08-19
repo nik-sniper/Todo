fdd03134ea4d3f68c3eca2dad9d4cd146b1d7348
Nik_Sniper <nik_mozir@mail.ru>
2019-08-19 17:26:03 +0300

    Анимация для тостов, добавил методы fadeIn and fadeOut

--- js/todo/Toast.js
+++ js/todo/Toast.js
@@ -10,21 +10,48 @@ class Toast {
     }
 
     show(text) {
-        this.container.innerHTML += `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                                        <div class="toast-header">
-                                            <strong class="mr-auto">${this.header}</strong>
-                                            <small>11 mins ago</small>
-                                            <button type="button" class="ml-2 close-toast" data-dismiss="toast" aria-label="Close">
-                                                <span aria-hidden="true">×</span>
-                                            </button>
-                                        </div>
-                                        <div class="toast-body">
-                                            ${text}
-                                        </div>`;
-        setTimeout(function () {
-            let toast = document.querySelector(".toast");
-            toast.parentElement.removeChild(toast);
-        },2000);
+        let toast = document.createElement("div");
+        toast.className = "toast";
+        this.container.appendChild(toast);
+        toast.innerHTML = `<div class="toast-header">
+                               <strong class="mr-auto">${this.header}</strong>
+                               <button type="button" class="ml-2 close-toast" data-dismiss="toast" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                               </button>
+                            </div>
+                            <div class="toast-body">
+                                 ${text}
+                            </div>`;
+        this.fadeIn(toast);
+    }
+
+    fadeIn(elem) {
+        let me = this;
+        let op = 0.1;
+        let timer = setTimeout(function animation() {
+            elem.style.opacity = op;
+            op += op * 0.1;
+            if (op >= 1) {
+                setTimeout(function () {
+                    me.fadeOut(elem);
+                },2000);
+            } else {
+                timer = setTimeout(animation,10);
+            }
+        }, 10)
+    }
+
+    fadeOut(elem) {
+        let op = 1;
+        let timer = setTimeout(function animation() {
+            elem.style.opacity = op;
+            op -= op * 0.1;
+            if (op <= 0.1) {
+                elem.parentElement.removeChild(elem);
+            } else {
+                timer = setTimeout(animation,10);
+            }
+        }, 10)
     }
 
     close(e) {
